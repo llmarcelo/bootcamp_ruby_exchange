974bd02bf7e39ae320d9f5e2cfb3ccf3a54f0701
llmarcelo <marcelosousa237.ms@gmail.com>
2020-08-08 10:29:05 -0300

    File process

--- lib/app.rb
+++ lib/app.rb
@@ -1,7 +1,9 @@
 require_relative 'file_process/file_process'
 
 file_path = 'assets/investiments.txt'
-data = 'Fazendo um teste'
+data = 'Fazendo um teste\n'
+line = 'Uma nova linha no arquivo\n'
 
 FileProcess.process_file(file_path, data)
 FileProcess.read_file(file_path)
+FileProcess.add_line(file_path, line)
--- lib/file_process/file_process.rb
+++ lib/file_process/file_process.rb
@@ -1,4 +1,8 @@
 class FileProcess
+    def self.add_line(file_path, line)
+        File.open(file_path, 'a') {|f| f.write(line)}
+    end
+    
     def self.read_file(file_path)
         File.foreach(file_path) {|line| puts line}
     end
